442ec0015fd6f43c97bc6851612f62af2215d550
GamedevForge <62645756+redHurt96@users.noreply.github.com>
2025-04-18 17:07:07 +0300

    Initial commit

---
+++ Assets/_Project/Logic/Infrastructure/MainSceneInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace _Project.Logic.Infrastructure
+{
+    public class MainSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
---
+++ Assets/_Project/Logic/Infrastructure/ProjectInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace _Project.Logic.Infrastructure
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
